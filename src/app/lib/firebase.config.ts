c1f2368c4566a4fbd1ec048082b34540484f0082
Joa Pozzo <pozzojoa@gmail.com>
2025-11-17 18:30:41 -0300

    logica completa de auth/login/register/forgotpassword

--- src/app/lib/firebase.config.ts
+++ src/app/lib/firebase.config.ts
@@ -2,14 +2,14 @@ import { initializeApp, getApps, FirebaseApp, FirebaseOptions } from 'firebase/a
 import { getAuth, Auth, setPersistence, browserLocalPersistence } from 'firebase/auth';
 import { getStorage, FirebaseStorage } from 'firebase/storage';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAhEO_mUUcOz_biVKcqOrz2Q6xj7LwHJ9A",
-  authDomain: "maxshop-86609.firebaseapp.com",
-  projectId: "maxshop-86609",
-  storageBucket: "maxshop-86609.firebasestorage.app",
-  messagingSenderId: "94563935971",
-  appId: "1:94563935971:web:ab33cd599e8be2a9ba87d6",
-  measurementId: "G-KH58135XXY"
+const firebaseConfig: FirebaseOptions = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyAhEO_mUUcOz_biVKcqOrz2Q6xj7LwHJ9A",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "maxshop-86609.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "maxshop-86609",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "maxshop-86609.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "94563935971",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:94563935971:web:ab33cd599e8be2a9ba87d6",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-KH58135XXY"
 };
 
 // Inicializar Firebase solo una vez
